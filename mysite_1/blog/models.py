cc8f2c0a94a71a669e4747b9aa20fb8e57f6e4f4
mehrdad <hafezyhafez7@gmail.com>
2025-05-29 18:54:14 +0330

    feat: add created updated, publshed datetime fileds to post model

--- mysite_1/blog/models.py
+++ mysite_1/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class post(models.Model):
     title = models.CharField(
@@ -12,6 +12,23 @@ class post(models.Model):
     body = models.TextField(
 
     )
+    published = models.DateTimeField(
+        default=timezone.now
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(
+                fields=['-published'],
+            )
+        ]
+
 
     def __str__(self):
         return self.title
