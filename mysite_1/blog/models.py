04d5c47a0c8e9dc43d2d4a3b6ef2115acb43a7aa
mehrdad <hafezyhafez7@gmail.com>
2025-06-10 20:40:16 +0330

    git commit -m 'feat: add status and author fields ti post model

--- mysite_1/blog/models.py
+++ mysite_1/blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
 class post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = ('DF','Draft')
+        PUBLISHED = ('PB','Published')
     title = models.CharField(
         max_length=250,
     )
@@ -12,14 +17,24 @@ class post(models.Model):
     body = models.TextField(
 
     )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='blog_post',
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
     published = models.DateTimeField(
-        default=timezone.now
+        default=timezone.now,
     )
     created = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
     updated = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
     )
     class Meta:
         ordering = ['-published']
