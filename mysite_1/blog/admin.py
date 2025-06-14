ed873692ed414f3006c0ee6d9aad126ee5130527
mehrdad <hafezyhafez7@gmail.com>
2025-06-14 21:00:28 +0330

    feat: add post model to admin

--- mysite_1/blog/admin.py
+++ mysite_1/blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import post
 # Register your models here.
+@admin.register(post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'author',
+        'published',
+        'status',
+    )
+    list_filter = (
+        'status',
+        'created',
+        'published',
+        'author',
+    )
+    search_fields = (
+        'title',
+        'body',
+    )
+    prepopulated_fields  = {
+        'slug': ('title',)
+    }
+    date_hierarchy = 'published'
+    ordering = (
+        '-status',
+        '-published'
+    )
