92bdc6c8dd5844e5c98cb8c6b0cad5eb380a52de
mehrdad <hafezyhafez7@gmail.com>
2025-06-24 18:42:41 +0330

    fix: pagination exeption

--- mysite_1/blog/views.py
+++ mysite_1/blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.http import Http404 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def post_list(request):
     posts = Post.pub.all()
 
     paginator = Paginator(posts,2)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(
         request,
         'blog/post/list.html',
