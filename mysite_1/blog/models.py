3dd8a8c8234fde8a95668bc5d1229d272a6178fc
mehrdad <hafezyhafez7@gmail.com>
2025-06-25 14:08:56 +0330

    feat: add comment model

--- mysite_1/blog/models.py
+++ mysite_1/blog/models.py
@@ -72,3 +72,26 @@ class Post(models.Model):
         return self.title
     
     
+class comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    name = models.CharField(
+        max_length=64
+    )
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = [
+            '-created',
+        ]
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        def __str__(self):
+            return f'comment by {self.name} on {self.post}'
