674de9402804f4af14f65280459a70baee5d4726
mehrdad <hafezyhafez7@gmail.com>
2025-07-01 11:14:26 +0330

    feat: switch db to postgresql

--- mysite_1/mysite_1/settings.py
+++ mysite_1/mysite_1/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'mysite_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
     }
 }
 
